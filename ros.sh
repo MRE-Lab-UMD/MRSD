9668578799cedbe68c3839a40aaff86ad3121e64
biggunray <58670940+biggunray@users.noreply.github.com>
2021-08-09 17:43:38 -0400

    Update ros.sh

--- ros.sh
+++ ros.sh
@@ -13,6 +13,6 @@ cd
 cd ~/catkin_ws/src
 git clone https://github.com/ros-industrial/ur_msgs.git
 git clone https://github.com/UniversalRobots/Universal_Robots_ROS_Driver.git 
-cd ..
+cd 
 source ~/catkin_ws/devel/setup.bash
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
