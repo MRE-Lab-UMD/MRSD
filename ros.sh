2f249116d7af66266ed473783c3130d3a76d0be5
biggunray <58670940+biggunray@users.noreply.github.com>
2021-08-09 17:40:17 -0400

    Update ros.sh

--- ros.sh
+++ ros.sh
@@ -4,7 +4,13 @@ sudo apt-get install ros-melodic-control*
 sudo apt-get install ros-melodic-ros-control*
 sudo apt install ros-melodic-rqt-joint-trajectory-controller
 
-cd ~catkin_ws/src
+source /opt/ros/noetic/setup.bash
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
+cd
+
+cd ~/catkin_ws/src
 git clone https://github.com/ros-industrial/ur_msgs.git
 git clone https://github.com/UniversalRobots/Universal_Robots_ROS_Driver.git 
 cd ..
