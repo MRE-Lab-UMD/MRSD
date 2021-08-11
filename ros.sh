3a685c60ed9763d70cea89ac64b71d3e9ed04404
biggunray <58670940+biggunray@users.noreply.github.com>
2021-08-11 15:50:45 -0400

    Update ros.sh

--- ros.sh
+++ ros.sh
@@ -7,13 +7,15 @@ sudo apt-get install ros-melodic-ur-client-library
 sudo apt-get install ros-melodic-industrial-robot-status-interface
 sudo apt install ros-melodic-ddynamic-reconfigure
 
-source /opt/ros/noetic/setup.bash
-mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/
-catkin_make
-cd
+source /opt/ros/melodic/setup.bash
+mkdir -p catkin_ws/src && cd catkin_ws/src
 
-cd ~/catkin_ws/src
 git clone https://github.com/ros-industrial/ur_msgs.git
 git clone https://github.com/UniversalRobots/Universal_Robots_ROS_Driver.git 
+git clone -b calibration_devel https://github.com/fmauch/universal_robot.git 
+cd ..
+sudo apt update -qq
+rosdep update
+rosdep install --from-paths src --ignore-src -y
+catkin_make
 cd
