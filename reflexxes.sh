1fabb76a100a1ee67b7d3537d41c7d5e7e723df8
biggunray <58670940+biggunray@users.noreply.github.com>
2021-08-10 23:59:53 -0400

    Update reflexxes.sh

--- reflexxes.sh
+++ reflexxes.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 cd MRSD_APPS
 git clone https://github.com/biggunray/reflexxes_type2.git
-cd reflexxes_type2
-mkdir build
-cd build
+cd reflexxes_type2/build
 cmake ..
 sudo make install
 cd
