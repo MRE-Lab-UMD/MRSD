c3c12cf959ba37114554d9b9dbf0a110c5f60ffc
biggunray <58670940+biggunray@users.noreply.github.com>
2021-08-26 21:39:12 -0400

    Update kdl.sh

--- kdl.sh
+++ kdl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get install libeigen3-dev libcppunit-dev
-mkdir MRSD_APPS
+
 cd MRSD_APPS
 git clone https://github.com/orocos/orocos_kinematics_dynamics.git
 cd orocos_kinematics_dynamics/orocos_kdl
